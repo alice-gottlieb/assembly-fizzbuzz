39b1fa7af2cf171f2c507cc7c5366daa22480bc9
alice-gottlieb <aliceagottlieb@gmail.com>
2022-07-26 00:36:30 +0100

    Single digit fizzbuzz!

--- fizzbuzz.asm
+++ fizzbuzz.asm
@@ -4,13 +4,12 @@
 
 %macro print 2 
    ; print msg %1 of length %2
-   MOV  EAX, 4
-   MOV  EBX, 1
    MOV  ECX, %1
    MOV  EDX, %2
+   MOV  EAX, 4
+   MOV  EBX, 1
    INT  0x80
 %endmacro
-
 
 %macro modByte 3
    ; Take %1 mod %2, send result to %3
@@ -31,16 +30,27 @@ section .bss
    b resb 4 
    c resb 4 
    d resb 4 
-   ascii resb 4
    rem resb 1 ; remainder of mod
    n resw 1
+   printWord resw 1
 
 section .data       
     lb db 0xA, 0xD
+    fizzNum db 2
+    buzzNum db 3
+    fizzMsg db 'fizz' ;, 0xA, 0xD
+    buzzMsg db 'buzz' ;, 0xA, 0xD
+    len equ $ - buzzMsg
 
 _start:
-    MOV ECX, 5 ; set number of loops
-    MOV EAX, 35 ; hold characaters for the numbers
+    MOV ECX, 9 ; set number of loops
+    MOV EAX, 1 ; hold characaters for the numbers
+
+    CALL fizzbuzz
+
+    MOV EAX, 1
+    INT 0x80
+    HLT
 
     modByte 78, 10, [rem]
     MOV AH, [rem]
@@ -73,10 +83,10 @@ numToAscii:
    ; Tens plae - CH
    ; Ones place - CL
 
-   MOV [a], EAX
-   MOV [b], EBX
-   MOV [c], ECX
-   MOV [d], EDX
+   ;MOV [a], EAX
+   ;MOV [b], EBX
+   ;MOV [c], ECX
+   ;MOV [d], EDX
    MOV [n], AL
 
    CMP AL, 10
@@ -104,20 +114,53 @@ numToAscii:
      ADD CL, 0x30
      RET
 
-looper:
-   MOV [num], EAX ; move only higher byte of EAX
-   MOV EDX, 1
-   MOV EBX, 1
-   PUSH ECX
-   MOV ECX, num ; move only 1 byte from num into ECX
-   MOV EAX, 4 ; sys_write command
-   INT 0x80 ; print
-   CALL linebreak
-   
-   MOV EAX, [num]
-   SUB EAX, 0x30
-   INC EAX
-   ADD EAX, 0x30
-   POP ECX ; retrieve old ECX value
-   LOOP looper
-   RET
+fizzbuzz:
+   ;MOV [num], EAX ; move only higher byte of EAX
+   MOV [c], ECX
+   MOV [a], EAX 
+   modByte [a], [fizzNum], [rem]
+   CMP byte [rem], 0
+   JE fizz
+   modByte [a], [buzzNum], [rem]
+   CMP byte [rem], 0
+   JE buzz
+
+   MOV EAX, [a]
+   CMP EAX, 10
+   JL oneDigit
+   CALL numToAscii
+   MOV [n], CH
+   MOV [rem], CL
+   JMP numPrint
+
+   numPrint:
+      print n, 1
+      print rem, 1
+      JMP cont
+
+   oneDigit:
+      ADD EAX, 0x30
+      MOV [printWord], EAX
+      print printWord, 1
+      JMP cont 
+
+   cont:
+      CALL linebreak
+      MOV EAX, [a]
+      INC EAX
+      MOV [a], EAX
+      MOV ECX, [c]
+      DEC ECX
+      MOV [c], ECX
+      CMP ECX, 0 
+      JGE fizzbuzz 
+      RET
+   fizz:
+      print fizzMsg, len
+      modByte [a], [buzzNum], [rem]
+      CMP byte [rem], 0
+      JE buzz
+      JMP cont
+   buzz:
+      print buzzMsg, len
+      JMP cont
